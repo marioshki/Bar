0fd5b841f375678b502eb13f249f2abf964c3581
Mario <marioemepe@gmail.com>
2014-04-14 11:56:32 +0200

    angular + foundation + static content

--- www/main.js
+++ www/main.js
@@ -2,37 +2,28 @@ var express = require('express');
 var jade = require('jade');
 var app = express();
 
+
+app.use("/static", express.static(__dirname + '/static'));
+
 app.engine('jade', jade.__express);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
-  res.render('index', {
-  	titulo:'Hola Mundo',
-  	parrafo:'Esto es un Hola Mundo'
-  });
+  res.render('index');
 });
 
 app.get('/productos', function(req, res){
-  res.render('productos', {
-  	titulo:'Hola Mundo',
-  	parrafo:'Esto es un Hola Mundo'
-  });
+  res.render('productos');
 });
 
 app.get('/oferta', function(req, res){
-  res.render('oferta', {
-  	titulo:'Hola Mundo',
-  	parrafo:'Esto es un Hola Mundo'
-  });
+  res.render('oferta');
 });
 
 app.get('/contacto', function(req, res){
-  res.render('contacto', {
-  	titulo:'Hola Mundo',
-  	parrafo:'Esto es un Hola Mundo'
-  });
+  res.render('contacto');
 });
 
 
