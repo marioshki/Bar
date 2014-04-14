c95020db77cf6fa5d653a1bbf3e57c0e3ab6802a
Mario <marioemepe@gmail.com>
2014-04-14 11:33:38 +0200

    created more views

--- www/main.js
+++ www/main.js
@@ -8,10 +8,33 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
-  res.render('plantilla', {
+  res.render('index', {
   	titulo:'Hola Mundo',
   	parrafo:'Esto es un Hola Mundo'
   });
 });
+
+app.get('/productos', function(req, res){
+  res.render('productos', {
+  	titulo:'Hola Mundo',
+  	parrafo:'Esto es un Hola Mundo'
+  });
+});
+
+app.get('/oferta', function(req, res){
+  res.render('oferta', {
+  	titulo:'Hola Mundo',
+  	parrafo:'Esto es un Hola Mundo'
+  });
+});
+
+app.get('/contacto', function(req, res){
+  res.render('contacto', {
+  	titulo:'Hola Mundo',
+  	parrafo:'Esto es un Hola Mundo'
+  });
+});
+
+
 
 app.listen(20001);
