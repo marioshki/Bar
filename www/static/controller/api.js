ca755b5f6f5242d4db82c3fd02d768841fb73398
Mario <marioemepe@gmail.com>
2014-04-23 17:37:59 +0200

    Some Changes

--- www/static/controller/api.js
+++ www/static/controller/api.js
@@ -104,5 +104,14 @@ app.run(function($rootScope) {
 });
 
 $(document).ready(function(){
-	$(document).foundation();
+	$(document).foundation({
+		orbit: {
+			animation: 'slide',
+			timer_speed: 1000,
+			pause_on_hover: true,
+			animation_speed: 500,
+			navigation_arrows: false,
+			bullets: false
+		}
+	});
 });
